b51bd6c6917fe3a83e3db029437639fdedd33653
Matundu Jules-Langa <63888057+Matundu-Jules@users.noreply.github.com>
2025-06-14 09:24:09 +0200

    feat: add channel membership management

--- supchat-server/routes/channel.Routes.js
+++ supchat-server/routes/channel.Routes.js
@@ -5,6 +5,9 @@ const {
   getChannelById,
   updateChannel,
   deleteChannel,
+  inviteToChannel,
+  joinChannel,
+  leaveChannel,
 } = require("../controllers/channelController");
 const { authMiddleware } = require("../middlewares/authMiddleware");
 const { validate } = require("../middlewares/validationMiddleware");
@@ -12,6 +15,7 @@ const {
   createChannelSchema,
   updateChannelSchema,
   channelIdParamSchema,
+  inviteToChannelSchema,
 } = require("../validators/channelValidators");
 
 const router = express.Router();
@@ -40,6 +44,27 @@ router.delete(
   authMiddleware,
   validate({ params: channelIdParamSchema }),
   deleteChannel
+);
+
+router.post(
+  "/:id/invite",
+  authMiddleware,
+  validate({ params: channelIdParamSchema, body: inviteToChannelSchema }),
+  inviteToChannel
+);
+
+router.post(
+  "/:id/join",
+  authMiddleware,
+  validate({ params: channelIdParamSchema }),
+  joinChannel
+);
+
+router.post(
+  "/:id/leave",
+  authMiddleware,
+  validate({ params: channelIdParamSchema }),
+  leaveChannel
 );
 
 module.exports = router;
