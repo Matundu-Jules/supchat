70db6c395a2e74d600e17d2648673264d0f371c3
Matundu Jules-Langa <63888057+Matundu-Jules@users.noreply.github.com>
2025-06-14 03:58:35 +0200

    feat(web): add loader component and use it across pages

--- client-web/src/components/layout/PublicRoute/index.tsx
+++ client-web/src/components/layout/PublicRoute/index.tsx
@@ -3,13 +3,14 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "@store/store";
+import Loader from "@components/Loader";
 
 const PublicRoute: React.FC = () => {
   const user = useSelector((state: RootState) => state.auth.user);
   const authLoading = useSelector((state: RootState) => state.auth.isLoading);
 
   if (authLoading) {
-    return <div className="loading">Chargement...</div>;
+    return <Loader />;
   }
 
   // If the user is logged in => redirect to dashboard (or other)
