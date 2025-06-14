d0fd50fdd6e89964fec7b03d5844e95cb2bc20b8
Matundu Jules-Langa <63888057+Matundu-Jules@users.noreply.github.com>
2025-06-14 22:40:06 +0200

    test: update messages endpoint

--- supchat-server/tests/message.test.js
+++ supchat-server/tests/message.test.js
@@ -3,12 +3,12 @@ const app = require("../src/app");
 
 describe("Test des routes Messages", () => {
   it("Envoie un message", async () => {
-    const res = await request(app).post("/api/messages/send").send({
-      content: "Bonjour tout le monde!",
+    const res = await request(app).post("/api/messages").send({
+      text: "Bonjour tout le monde!",
       channelId: "1234567890",
     });
 
     expect(res.statusCode).toBe(201);
-    expect(res.body.content).toBe("Bonjour tout le monde!");
+    expect(res.body.text).toBe("Bonjour tout le monde!");
   });
 });
